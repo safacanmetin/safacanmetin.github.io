9c25eee7124e3ac0fc2b65dff4deb8cdd398c6b7
Safacan Metin <safacanmetin@gmail.com>
2020-12-19 14:03:44 +0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function myFunction() {
     var quotes = ["Durmak ölüm, taklit uşaklıktır, çalışmak ve yetişmek ise hayat ve hürriyettir.",
+    "Akılsız başın cezasını ayaklar çeker.",
     "Be the change that you wish to see in the world. Mahatma Gandhi",
-    "The more you learn, the higher you'll see, The quicker you grow, the stronger you'll be. Robin Sharma"    
-     
-        ];
+    "The more you learn, the higher you'll see, The quicker you grow, the stronger you'll be. Robin Sharma" ,   
+    "Wealth is the slave of wise man. The master of a fool.",
+    "There are no shortcuts to any place worth going.",    
+    "If you have no critics you’ll likely have no success.",
+    "If you want to shine like the sun, first burn like the sun.",
+    "If it is important to you, you will find a way. If not, you’ll find an excuse.",
+
+
+    ];
 
     const random = Math.floor(Math.random() * quotes.length);
 
